cbb69da8433ce4222d8cba7fca351ea27b7d39d8
Prethush <ktmirash49@gmail.com>
2021-07-05 10:04:46 +0530

    CRUD

--- block-BNaacu/students/app.js
+++ block-BNaacu/students/app.js
@@ -1,14 +1,22 @@
+
 let express = require('express');
 let studentsRouter = require('./routes/students');
 let app = express();
 
 //middleware
-//setup template engine
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 app.use('/students', studentsRouter)
+
+//error handler
+
+app.use((req, res, next) => {
+    res.status(404).send("Page not found");
+});
 
 app.listen(3000, () => {
     console.log("Server islistening on port 3k");
--- block-BNaacu/students/routes/students.js
+++ block-BNaacu/students/routes/students.js
@@ -2,11 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/new', (req, res)  => {
-    
+    res.render('studentForm');
 });
 
 router.post('/', (req, res) => {
-
+    res.send('Stdent data saved');
 });
 
 router.get('/', (req, res) => {
@@ -17,6 +17,6 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     let students = {name: "ankit", email: "ankit@gmail.com"};
     res.render("studentDetail", {students: students});
-     
+
 })
 module.exports = router;
